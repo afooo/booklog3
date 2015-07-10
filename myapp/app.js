a906421f82df2edd238af12ae8d1d3749c19ac55
Afoo Chang <foofoo.chang@gmail.com>
2015-07-10 22:38:15 +0800

    install express-session

--- myapp/app.js
+++ myapp/app.js
@@ -5,13 +5,15 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var session = require('express-session');
 var passport = require('passport');
 
-var FacebookStrategy = require('passport-facebook');
+var FacebookStrategy = require('passport-facebook').Strategy;
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var posts = require('./routes/posts');
+var account = require('./routes/account');
 
 mongoose.connect('mongodb://booklog3:123456@ds047622.mongolab.com:47622/booklog3');
 mongoose.connection.on('error', function(){
@@ -31,6 +33,30 @@ var postSchema = new Schema({
 
 var Post = mongoose.model('post', postSchema);
 
+var app = express();
+
+app.db = {
+  model: {
+    Post: Post
+  }
+};
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'booklog3' }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 passport.serializeUser(function(user, done){
   done(null, user);
 });
@@ -42,45 +68,26 @@ passport.deserializeUser(function(user, done){
 passport.use(new FacebookStrategy({
   clientID: '410866279063864',
   clientSecret: '3887b8914b81d0e778d3b9af10775fb6',
-  callbackURL: 'http://localhost:3000/'
+  callbackURL: 'http://localhost:3000/auth/facebook/callback'
   },
   function(accessToken, refreshToken, profile, done){
-    done(null, profile);
+    return done(null, profile);
   }
 ));
-
-var app = express();
-
-app.db = {
-  model: {
-    Post: Post
-  }
-};
-
-app.get('/auth/facebook',
-  passport.authenticate('facebook'));
-
-app.get('auth/facebook/callback',
-  passport.authenticate('facebook', { failureRedirect: '/login' }),
-  function(req, res){
-    res.redirect('/');
-  });
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/', posts);
 app.use('/users', users);
+app.use('/account', account);
+
+app.get('/auth/facebook',
+  passport.authenticate('facebook'));
+
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res){
+    res.redirect('/account/profile');
+  });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

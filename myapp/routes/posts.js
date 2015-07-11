f23c610ba2949446b456fe496d99a9073039047a
Afoo Chang <foofoo.chang@gmail.com>
2015-07-11 21:55:22 +0800

    install jade and add post statusmachine

--- myapp/routes/posts.js
+++ myapp/routes/posts.js
@@ -4,7 +4,7 @@ var events = require('events');
 
 function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) { return next(); }
-  res.redirect('/login/facebook');
+  res.redirect('/auth/facebook');
 }
 
 router.get('/1/post', ensureAuthenticated);
@@ -17,6 +17,7 @@ router.get('/1/post', function(req, res, next) {
 	});
 });
 
+router.get('/1/post', ensureAuthenticated);
 router.get('/1/post/:id', function(req, res, next) {
 	req.app.db.model.Post.findOne({ _id: req.params.id }, function(err, post){
 		res.json(post);
@@ -30,14 +31,63 @@ router.get('/1/title/:title', function(req, res, next) {
 });
 
 router.post('/1/post', function(req, res, next) {
-	var post = new req.app.db.model.Post({
-		title: "July 10",
-		content: "typhoon arriving"
+	var workflow = new events.EventEmitter();
+	var Post  = req.app.db.model.Post;
+
+	workflow.outcome = {
+		isAuth: false,
+		success: false,
+		errfor: {}
+	};
+
+	workflow.on('authenticate', function(){
+		if(!req.isAuthenticated()){
+			workflow.outcome.errfor = 'authenticate fail';
+			res.redirect('/auth/facebook');
+		}
+
+		workflow.outcome.isAuth = true;
+		workflow.emit('validate');
 	});
 
-	post.save();
+	workflow.on('validate', function(){
+		if(typeof(req.query.title) === 'undefined' 
+			|| req.query.title.length === 0)
+			workflow.outcome.errfor.title = '請填寫標題哦';
 
-	res.json(post);
+		if(typeof(req.query.content) === 'undefined' 
+			|| req.query.content.length === 0)
+			workflow.outcome.errfor.content = '內容不可空白啦';
+
+		if(Object.keys(workflow.outcome.errfor).length !== 0) {
+				workflow.outcome.success = false;
+				workflow.emit('response');
+		}
+
+		workflow.emit('savePost');
+	});
+
+	workflow.on('savePost', function(){
+		var obj = new Post({
+			title: req.query.title,
+			content: req.query.content,
+			userId: req.user._id
+		});
+
+		obj.save(function(err, post){
+			workflow.outcome.success = true;
+			console.log('save was successful');
+			workflow.outcome.post = post;
+
+			workflow.emit('response');
+		});
+	});
+
+	workflow.on('response', function(){
+		res.send(workflow.outcome);
+	});
+
+	workflow.emit('authenticate');
 });
 
 router.put('/1/post/:id', function(req, res, next){
